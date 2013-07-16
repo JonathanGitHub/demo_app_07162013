2d3815f41c452e38613da127dc395e9b2a9e923d
JonathanGitHub <jonathan_education@yahoo.com>
2013-07-16 16:42:16 -0700

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+DemoApp07162013::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp07162013::Application.config.secret_token = 'a8757caf0739916ae4c106d015fb8d6cf5c92e7670156accdc8bc00c24bbd1fd9782eadcfa16967b664bc0c4f3fbf46a694594d856d0b535a654a37bc384ec64'
